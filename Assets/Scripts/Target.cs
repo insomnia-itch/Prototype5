4e13667144edac012413e614375da0edb55d3c84
Jelani Woods <jelani@firstdraft.com>
2021-09-03 21:46:44 -0500

    Cleanup script

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
